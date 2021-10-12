9caaae65194252a222494d6d7a58af73976e0b89
Marcus <mvsilva1997@gmail.com>
2021-10-12 12:37:33 -0300

    remove tests

--- src/tweets/tweets.module.ts
+++ src/tweets/tweets.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TweetsService } from './tweets.service';
 import { TweetsController } from './tweets.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  providers: [TweetsService],
+  providers: [TweetsService, PrismaService],
   controllers: [TweetsController],
 })
 export class TweetsModule {}
