903e1846138ad77e9c9719857c0ad81590acae25
Marcus <mvsilva1997@gmail.com>
2021-10-12 14:13:06 -0300

    update tweet routes

--- src/tweets/tweets.controller.ts
+++ src/tweets/tweets.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Delete,
+  Param,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { CreateTweetDto } from './tweet.dto';
+import { Tweet } from '.prisma/client';
+import { TweetsService } from './tweets.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('tweets')
-export class TweetsController {}
+export class TweetsController {
+  constructor(private service: TweetsService) {}
+
+  @UseGuards(AuthGuard('jwt'))
+  @UsePipes(ValidationPipe)
+  @Post('create')
+  async createTweet(@Body() createTweet: CreateTweetDto): Promise<Tweet> {
+    return this.service.createTweet(createTweet);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @UsePipes(ValidationPipe)
+  @Delete('delete/:id')
+  async deleteTweet(@Param('id', ParseIntPipe) id: number): Promise<Tweet> {
+    return this.service.deleteTweet({ id: id });
+  }
+}
--- src/tweets/tweets.service.ts
+++ src/tweets/tweets.service.ts
@@ -1,12 +1,27 @@
 import { Tweet, Prisma } from '.prisma/client';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateTweetDto } from './tweet.dto';
 
 @Injectable()
 export class TweetsService {
   constructor(private db: PrismaService) {}
 
-  async create(data: Prisma.TweetCreateInput): Promise<Tweet> {
+  async createTweet(data: CreateTweetDto): Promise<Tweet> {
     return this.db.tweet.create({ data });
+  }
+
+  async deleteTweet(where: Prisma.TweetWhereUniqueInput): Promise<Tweet> {
+    const { id } = where;
+
+    const tweet = await this.db.tweet.findUnique({
+      where: { id },
+    });
+
+    if (!tweet) {
+      throw new NotFoundException('tweet_not_found');
+    }
+
+    return this.db.tweet.delete({ where: { id } });
   }
 }
